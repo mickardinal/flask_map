4e4dab9c8b47cab94f44cbba812e54c0dbced5a2
Shawn Chan <jsrdccsx@gmail.com>
2018-03-25 12:52:48 +0800

    first

---
+++ routes.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from forms import SignupForm
+
+app = Flask(__name__)
+
+app.secret_key = "development-key"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
