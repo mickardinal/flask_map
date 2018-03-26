92b8db8fc1ae2315efc898c6c927f4d5e4d8d634
Shawn Chan <jsrdccsx@gmail.com>
2018-03-26 15:40:42 +0800

    log finished

--- routes.py
+++ routes.py
@@ -67,5 +67,10 @@ def login():
         return render_template( 'login.html', form=form)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
